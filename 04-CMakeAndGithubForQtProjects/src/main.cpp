1d0f2394224268d56326431c785353659dfc2f8f
Nasser GHOSEIRI <nasser.ghoseiri@gmail.com>
2026-04-19 15:50:35 +0200

    Compile and test now running

--- 04-CMakeAndGithubForQtProjects/src/main.cpp
+++ 04-CMakeAndGithubForQtProjects/src/main.cpp
@@ -1,13 +1,21 @@
 #include "hello.h"
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
-
-    qDebug() << getHelloMessage();
+    /* Note: Usually the following line is what you would be putting here,
+             but since all we want to do it output a message, we remove this.
+    
+             Otherwise, the app will invisibly running in the background, and you would have
+             to go in an manually kill the process.
+    */
 
     // TODO: Replace with QWidget-based GUI when you extend the template
-    return app.exec();
+    // QCoreApplication app(argc, argv);
+    // return app.exec();
+
+    std::cout << getHelloMessage().toStdString() << std::endl;
+    return 0;
 }
